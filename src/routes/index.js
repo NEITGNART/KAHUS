ca1134349162558f0f20d2e70e19f7d78699a0ed
Pham Tien <19127576@student.hcmus.edu.vn>
2022-11-22 00:19:09 +0700

    Adding Snackbar for verify email

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
-import { Suspense, lazy } from 'react';
+import { Suspense, lazy, useState } from 'react';
 import { Navigate, useRoutes, useLocation } from 'react-router-dom';
 // guard
+import PropTypes from 'prop-types';
+import { useSnackbar } from 'notistack';
 import AuthGuard from '../guards/AuthGuard';
 import HomePage from '../pages/Home';
 // layouts
@@ -11,7 +13,7 @@ import LoadingScreen from '../components/LoadingScreen';
 import { PATH_AFTER_LOGIN } from '../config';
 import DashboardLayout from '../layout/dashboard';
 import { ROOTS_DASHBOARD } from './paths';
-
+import useAuth from '../hooks/useAuth';
 // ----------------------------------------------------------------------
 
 const Loadable = (Component) =>
@@ -29,6 +31,22 @@ const Loadable = (Component) =>
     );
   };
 
+SuccessVerify.propTypes = {
+  children: PropTypes.node
+};
+
+function SuccessVerify({ children }) {
+  const { isInitialized } = useAuth();
+  const { enqueueSnackbar } = useSnackbar();
+
+  if (isInitialized) {
+    enqueueSnackbar('Verify success', {
+      variant: 'success'
+    });
+  }
+  return <>{children}</>;
+}
+
 export default function Router() {
   return useRoutes([
     {
@@ -37,6 +55,14 @@ export default function Router() {
         {
           path: 'login',
           element: <Login />
+        },
+        {
+          path: 'login/success',
+          element: (
+            <SuccessVerify>
+              <Login />
+            </SuccessVerify>
+          )
         },
         {
           path: 'register',
