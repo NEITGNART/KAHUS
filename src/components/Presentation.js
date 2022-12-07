d21da0eb2bba916a80a47d55164a4337a0799059
Pham Tien <19127576@student.hcmus.edu.vn>
2022-12-08 01:13:20 +0700

    Adding present slide for owner premier

---
+++ src/components/Presentation.js
@@ -0,0 +1,218 @@
+import React, { useEffect, useMemo, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import {
+  FlexBox,
+  Heading,
+  SpectacleLogo,
+  UnorderedList,
+  CodeSpan,
+  OrderedList,
+  ListItem,
+  FullScreen,
+  AnimatedProgress,
+  Appear,
+  Slide,
+  Deck,
+  Text,
+  Grid,
+  Box,
+  Image,
+  CodePane,
+  MarkdownSlide,
+  MarkdownSlideSet,
+  Notes,
+  SlideLayout,
+  Progress,
+  useSteps
+} from 'spectacle';
+import {
+  Button,
+  Card,
+  FormControl,
+  FormControlLabel,
+  FormHelperText,
+  FormLabel,
+  Radio,
+  RadioGroup,
+  Stack,
+  Stepper,
+  Typography
+} from '@mui/material';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js';
+
+// eslint-disable-next-line import/no-unresolved
+import { Bar } from 'react-chartjs-2';
+import { io } from 'socket.io-client';
+import { useSearchParams } from 'react-router-dom';
+import { useParams } from 'react-router';
+import { FormProvider } from './hook-form';
+import RHFMyRadioGroup from './hook-form/RHFMyRadioGroup';
+import { HOST_API } from '../config';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const options = {
+  plugins: {
+    legend: {
+      position: 'top'
+    }
+  },
+  scales: {
+    x: {
+      grid: {
+        display: false
+      }
+    },
+    y: {
+      grid: {
+        display: false
+      },
+      ticks: {
+        precision: 0
+      }
+    }
+  }
+};
+
+const socket = io(HOST_API);
+
+function Presentation() {
+  const [labels, setLabels] = useState([]);
+  const [numberAnswer, setNumberAnswer] = useState(labels.map(() => 0));
+  const [question, setQuestion] = useState('');
+  const [answer, setAnswer] = useState('');
+  const [error, setError] = useState(false);
+  const [helperText, setHelperText] = useState('Choose wisely');
+  const [searchParams, setSearchParams] = useSearchParams();
+  const { code } = useParams();
+
+  // get query params from url
+  const slideIndex = searchParams.get('slideIndex');
+  const roomCode = code || '123456';
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      socket.emit('join', { room: roomCode, slideIndex: Number(slideIndex) });
+
+      socket.on('chart', (data) => {
+        setQuestion(data.question);
+        setLabels(data.answer);
+        setNumberAnswer(data.numberAnswer);
+      });
+
+      socket.on('vote', (data) => {
+        setNumberAnswer(data.numberAnswer);
+      });
+    });
+
+    return () => {
+      socket.off('connect');
+      socket.off('disconnect');
+    };
+  }, []);
+
+  const datas = {
+    labels,
+    datasets: [
+      {
+        label: 'Votes',
+        data: numberAnswer,
+        backgroundColor: 'rgb(20, 86, 204)'
+      }
+    ]
+  };
+
+  const handleRadioChange = (event) => {
+    setAnswer(event.target.value);
+    setHelperText(' ');
+    setError(false);
+  };
+
+  const onSubmit = async (data) => {
+    socket.emit('answer', {
+      slideIndex,
+      answer
+    });
+    if (answer === '') {
+      setError(true);
+      setHelperText('Please select an option.');
+    } else {
+      setHelperText('Answer is submitted.');
+      setAnswer('');
+    }
+  };
+
+  const methods = useForm({
+    defaultValues: {
+      quiz: []
+    }
+  });
+
+  const {
+    handleSubmit,
+    formState: { errors, isSubmitting, isSubmitSuccessful }
+  } = methods;
+
+  return (
+    <Deck template={template}>
+      <Slide backgroundColor="white" slideNum={1}>
+        <Heading color="#212B36">{question}</Heading>
+        <FlexBox>
+          <Box height="100%" width="70%">
+            {labels.length > 0 ? (
+              <Bar options={options} data={datas} />
+            ) : (
+              <>Loading</>
+            )}
+          </Box>
+          <FormProvider
+            methods={methods}
+            onSubmit={handleSubmit(onSubmit)}
+            width="30%"
+          >
+            <RHFMyRadioGroup
+              onChange={handleRadioChange}
+              labels={labels}
+              value={answer}
+              error={error}
+              helperText={helperText}
+            />
+          </FormProvider>
+        </FlexBox>
+      </Slide>
+    </Deck>
+  );
+}
+
+const template = () => (
+  <FlexBox
+    justifyContent="space-between"
+    position="absolute"
+    bottom={0}
+    width={1}
+  >
+    <Box padding="0 1em">
+      <FullScreen color="#111213" />
+    </Box>
+    <Box padding="1em">
+      <Progress color="#111213" />
+    </Box>
+  </FlexBox>
+);
+
+export default Presentation;
